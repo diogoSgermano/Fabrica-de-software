d7307336463febd0233975664d1a9bd94c8b90aa
Diogo Soares Germano <diogogermano28@gmail.com>
2026-03-10 13:18:47 -0300

    Add Routes

--- ecorota/src/App.js
+++ ecorota/src/App.js
@@ -1,13 +1,46 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import './App.css';
+import './App.css'
+
+import Navbar from './components/layouts/Navbar'
+import Container from './components/layouts/Container'
+
+import Inicio from './pages/Inicio'
+import Sobre from './pages/Sobre'
+import Contato from './pages/Contato'
+import LocaisDeColeta from './pages/LocaisDeColeta'
+import ColetaDomiciliar from './pages/ColetaDomiciliar'
+import Denunciar from './pages/Denunciar'
+import TiposDeResiduos from './pages/TiposDeResiduos'
+import OutrasInformacoes from './pages/OutrasInformacoes'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Olá, mundo</h1>
-    </div>
-  );
+    <Router>
+
+      <Navbar />
+
+      <Container customClass="altura-minima">
+        <Routes>
+
+          <Route path="/" element={<Inicio />} />
+          <Route path="/sobre" element={<Sobre />} />
+
+          {/* SERVIÇOS */}
+          <Route path="/locais-de-coleta" element={<LocaisDeColeta />} />
+          <Route path="/coleta-domiciliar" element={<ColetaDomiciliar />} />
+          <Route path="/denunciar" element={<Denunciar />} />
+          <Route path="/tipos-de-residuos" element={<TiposDeResiduos />} />
+
+          <Route path="/contato" element={<Contato />} />
+          <Route path="/outras-informacoes" element={<OutrasInformacoes />} />
+
+        </Routes>
+      </Container>
+
+
+    </Router>
+  )
 }
 
-export default App;
+export default App
