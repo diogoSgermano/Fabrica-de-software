e3af541205969b79a2145f62239c763e56772b37
Diogo Soares Germano <diogogermano28@gmail.com>
2026-03-10 20:27:26 -0300

    Create Inicio

--- ecorota/src/pages/Inicio.jsx
+++ ecorota/src/pages/Inicio.jsx
@@ -1,6 +1,23 @@
+import ContainerCenter from "../components/layouts/ContainerCenter";
+import styles from "../pages/Inicio.module.css"
+import portal from "../../src/assets/images/portal-joinville-inicio.png"
+import LinkButton from "../components/LinkButton";
 function Inicio(){
   return(
-    <h1>Inicio</h1>
+    <>
+      <ContainerCenter>
+        <div>
+          <h1 className={styles.h1}>Sejam bem vindo(a) ao EcoRota</h1>
+          <h3 className={styles.h3}> Sistema de gestão de resíduos do município de Joinville</h3>
+        </div>
+      </ContainerCenter>
+      <div className={styles.portalContainer}>
+        <img className={styles.portalImage} src={portal} alt="portal de Joinville"></img>
+        <div className={styles.buttonOverlay}>
+          <LinkButton to="/locais-de-coleta" text="Para iniciar, clique aqui" />
+        </div>
+      </div>
+    </>
   );
 }
 
